5d73060beb1bdd03431b655067a714fc6d87dc63
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2024-06-12 10:48:20 +0530

    Update Validpara.java

--- stackdsa/Validpara.java
+++ stackdsa/Validpara.java
@@ -1,19 +1,29 @@
 import java.util.*;
 public class Validpara {
-    public static void main(String[] args) {
-        String Str="()[]{}";
+    public boolean isValid(String Str) {
         Stack<Character>s=new Stack<>();
         for(int i=0;i<Str.length();i++){
-            char x=Str.charAt(i);
-            if("({[".indexOf(x)>=0){
-              s.push(x);
-            }
-            else if(")}]".indexOf(x)>=0){
-                //char y=s.pop();
-   
-            }
+          char x=Str.charAt(i);
+          if("({[".indexOf(x)>=0){
+            s.push(x);
           }
-      
-      System.out.println(s.isEmpty());
+          else if(")}]".indexOf(x)>=0){
+            if(s.isEmpty()){
+                return false;
+            }
+              char y=s.pop();
+             if((y=='('&&x==')')||(y=='{'&&x=='}')||(y=='['&&x==']')){
+
+                }
+                else{
+                  return false;
+                }
+          }
+        }
+        return s.isEmpty();
+    }   
+    public static void main(String[] args) {
+        String Str="()[]{}";
+        System.out.print(isValid(str));
     }
 }
